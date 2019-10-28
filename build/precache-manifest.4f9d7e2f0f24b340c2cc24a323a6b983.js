b0e259a403d42dc246a9fbde40e702b40951123d
MHerrity <matthewherrity@gmail.com>
2019-10-28 15:00:32 -0400

    createreactapp

---
+++ build/precache-manifest.4f9d7e2f0f24b340c2cc24a323a6b983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8a1e88c96e636dfbbec16a6310116b",
+    "url": "/SpotifyProyectoDos/index.html"
+  },
+  {
+    "revision": "ce85614898e04e293390",
+    "url": "/SpotifyProyectoDos/static/css/main.0844f9ea.chunk.css"
+  },
+  {
+    "revision": "6f96f33afc7e0a2df4ce",
+    "url": "/SpotifyProyectoDos/static/js/2.1b01b0ff.chunk.js"
+  },
+  {
+    "revision": "ce85614898e04e293390",
+    "url": "/SpotifyProyectoDos/static/js/main.e6c19d26.chunk.js"
+  },
+  {
+    "revision": "f4186f8f6528c596f504",
+    "url": "/SpotifyProyectoDos/static/js/runtime-main.92363e50.js"
+  }
+]);
