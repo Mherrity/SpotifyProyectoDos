8218d769ee3721d88a22ad370e40cd2019904ade
MHerrity <matthewherrity@gmail.com>
2019-10-28 23:44:07 -0400

    Updates

--- precache-manifest.741fe6867b04b03eb501623bfc927558.js
+++ precache-manifest.741fe6867b04b03eb501623bfc927558.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f60db48e4a30359a1225a278f113609",
+    "revision": "ffe67c4873fb1c60db47d0941b3dda4f",
     "url": "/SpotifyProyectoDos/index.html"
   },
   {
-    "revision": "f11846ddde5c5194c2ce",
+    "revision": "ef3a81e3e43085e502f1",
     "url": "/SpotifyProyectoDos/static/css/main.0844f9ea.chunk.css"
   },
   {
-    "revision": "883b91a8ce57a3b91ab4",
-    "url": "/SpotifyProyectoDos/static/js/2.cf82b9ac.chunk.js"
+    "revision": "2db5cfd04df0f8b0073c",
+    "url": "/SpotifyProyectoDos/static/js/2.977ee073.chunk.js"
   },
   {
-    "revision": "f11846ddde5c5194c2ce",
-    "url": "/SpotifyProyectoDos/static/js/main.104a20e4.chunk.js"
+    "revision": "ef3a81e3e43085e502f1",
+    "url": "/SpotifyProyectoDos/static/js/main.9832a948.chunk.js"
   },
   {
     "revision": "f4186f8f6528c596f504",
